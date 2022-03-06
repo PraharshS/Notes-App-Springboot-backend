f3d42b41f3ead8be430c737d2099d5ccd31c6498
Praharsh Singh <praharsh535@gmail.com>
2022-03-06 21:47:18 +0530

    update notes

--- notes-app/src/main/java/net/project/springboot/Services/NoteService.java
+++ notes-app/src/main/java/net/project/springboot/Services/NoteService.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import net.project.springboot.models.Note;
 import net.project.springboot.models.User;
@@ -23,6 +25,13 @@ public class NoteService {
 
     public List<Note> getAllNotesByUser(User user) {
         return noteRepository.findByUserId(user.getId());
+    }
+
+    public ResponseEntity<Note> updateNote(@PathVariable Integer id, @RequestBody Note updatedNote) {
+        Note oldNote = noteRepository.findById(id).orElseThrow();
+        oldNote.setMessage(updatedNote.getMessage());
+        noteRepository.save(oldNote);
+        return ResponseEntity.ok(oldNote);
     }
 
     public ResponseEntity<Map<String, Boolean>> deleteNote(Integer id) {
--- notes-app/src/main/java/net/project/springboot/controllers/NotesController.java
+++ notes-app/src/main/java/net/project/springboot/controllers/NotesController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,11 @@ public class NotesController {
     @PostMapping("")
     public Note addNote(@RequestBody Note note) {
         return noteService.addNote(note);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Note> updateNote(@PathVariable Integer id, @RequestBody Note updatedNote) {
+        return noteService.updateNote(id, updatedNote);
     }
 
     @DeleteMapping("/{id}")
