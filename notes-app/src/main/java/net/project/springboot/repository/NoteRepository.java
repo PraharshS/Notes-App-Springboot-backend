3378dd0a64bbad741d1b65635ff0ae67eaac1859
Praharsh Singh <praharsh535@gmail.com>
2022-03-09 22:28:03 +0530

    fixed imports

--- notes-app/src/main/java/net/project/springboot/repository/NoteRepository.java
+++ notes-app/src/main/java/net/project/springboot/repository/NoteRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import net.project.springboot.models.Note;
-import net.project.springboot.models.User;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Integer> {
